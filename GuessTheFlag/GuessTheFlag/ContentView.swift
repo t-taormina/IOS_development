f2e24b7b959c8de16a33576edad058e86228327a
t-taormina <tktaormina@gmail.com>
2023-03-16 07:37:43 -0700

    Finished Guess the Flag. On section Why Does SwiftUI use structs for views?

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -9,41 +9,73 @@ import SwiftUI
 struct ContentView: View {
     
     @State private var showingScore = false
+    @State private var showingNewGame = false
     @State private var score = 0
+    @State private var attempts = 0
     @State private var scoreTitle = ""
     @State private var countries = ["France", "Germany", "Estonia", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
     @State private var correctAnswer = Int.random(in: 0...2)
     
     var body: some View {
         ZStack {
-            LinearGradient(gradient: Gradient(colors: [.blue, .black]), startPoint: .top, endPoint: .bottom)
-                .ignoresSafeArea()
-            VStack(spacing: 30) {
-                VStack {
-                    Text("Tap the flag of ")
-                        .foregroundColor(.white)
-                        .font(.subheadline.weight(.heavy))
-                    Text(countries[correctAnswer])
-                        .foregroundColor(.white)
-                        .font(.largeTitle.weight((.semibold)))
-                }
+            RadialGradient(stops: [
+                .init(color: Color(red: 0.1, green: 0.2, blue: 0.5), location: 0.3),
+                .init(color: Color(red: 0.76, green: 0.15, blue: 0.46), location: 0.3),
+            ], center: .top, startRadius: 400, endRadius: 700 )
+            .ignoresSafeArea()
+            
+            VStack {
+                Spacer()
                 
-                ForEach(0..<3) { number in
-                    Button {
-                        flagTapped(number)
-                    } label: {
-                        Image(countries[number])
-                            .renderingMode(.original)
-                            .clipShape(Rectangle())
-                            .shadow(color: Color(white: 0, opacity: 0.50) ,radius: 5)
+                Text("Guess the Flag")
+                    .font(.largeTitle.bold())
+                    .foregroundColor(.white)
+                
+                VStack(spacing: 15) {
+                    
+                    VStack {
+                        Text("Tap the flag of ")
+                            .foregroundStyle(.secondary)
+                            .font(.subheadline.weight(.heavy))
+                        Text(countries[correctAnswer])
+                            .font(.largeTitle.weight((.semibold)))
+                    }
+                    
+                    ForEach(0..<3) { number in
+                        Button {
+                            flagTapped(number)
+                        } label: {
+                            Image(countries[number])
+                                .renderingMode(.original)
+                                .clipShape(Rectangle())
+                                .shadow(color: Color(white: 0, opacity: 0.50) ,radius: 5)
+                        }
                     }
                 }
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, 20)
+                .background(.thinMaterial)
+                .clipShape(RoundedRectangle(cornerRadius: 20))
+                
+                Spacer()
+                Spacer()
+                
+                Text("Current score: \(score)")
+                    .foregroundColor(.white)
+                
+                Spacer()
             }
+            .padding()
         }
         .alert(scoreTitle, isPresented: $showingScore) {
             Button("Continue", action: askQuestion)
         } message: {
             Text("Your score is \(score)")
+        }
+        .alert("Game Over", isPresented: $showingNewGame) {
+            Button("Start New Game", action: reset)
+        } message: {
+            Text("You answered \(score)/8 flags correctly!")
         }
     }
     
@@ -51,18 +83,29 @@ struct ContentView: View {
         if number == correctAnswer {
             scoreTitle = "Correct"
             score += 1
+            attempts += 1
         } else {
-            scoreTitle = "Incorrect"
+            scoreTitle = "Incorrect. That's the flag of \(countries[number])"
+            attempts += 1
         }
-        showingScore = true
+        if attempts == 8 {
+            showingNewGame = true
+        }
+        else {
+            showingScore = true
+        }
     }
     
     func askQuestion() {
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
     }
+    
+    func reset() {
+        score = 0
+        attempts = 0
+    }
 }
-
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
